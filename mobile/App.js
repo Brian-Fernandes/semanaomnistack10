236f38001c250988c09ca19d174124d1320b76a2
unknown <brian12fernandes@gmail.com>
2020-01-17 14:39:08 -0300

    Terminar em casa

--- mobile/App.js
+++ mobile/App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App do Brian
-      </Text>
+      <Text style={styles.title}>Hello Omnistack..</Text>
     </View>
   );
 }
@@ -13,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: '#FFF'
   },
 });
